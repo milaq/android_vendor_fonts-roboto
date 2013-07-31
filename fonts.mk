752badae7cb748447a431531d15a13654b0985ef
milaq <micha.laqua@gmail.com>
2013-07-31 22:10:26 +0200

    initial commit

---
+++ fonts.mk
@@ -0,0 +1,8 @@
+# overwrite default roboto fonts
+PRODUCT_COPY_FILES += \
+       vendor/fonts-roboto/Roboto-Bold.ttf:system/fonts/Roboto-Bold.ttf \
+       vendor/fonts-roboto/Roboto-BoldItalic.ttf:system/fonts/Roboto-BoldItalic.ttf \
+       vendor/fonts-roboto/Roboto-Italic.ttf:system/fonts/Roboto-Italic.ttf \
+       vendor/fonts-roboto/Roboto-Light.ttf:system/fonts/Roboto-Light.ttf \
+       vendor/fonts-roboto/Roboto-LightItalic.ttf:system/fonts/Roboto-LightItalic.ttf \
+       vendor/fonts-roboto/Roboto-Regular.ttf:system/fonts/Roboto-Regular.ttf
