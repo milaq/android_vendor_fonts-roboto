48030a633d782ab9bc92a5a243268797f9eb03b9
milaq <micha.laqua@gmail.com>
2013-08-01 17:58:01 +0200

    add missing fonts

--- fonts.mk
+++ fonts.mk
@@ -5,4 +5,10 @@ PRODUCT_COPY_FILES += \
        vendor/fonts-roboto/Roboto-Italic.ttf:system/fonts/Roboto-Italic.ttf \
        vendor/fonts-roboto/Roboto-Light.ttf:system/fonts/Roboto-Light.ttf \
        vendor/fonts-roboto/Roboto-LightItalic.ttf:system/fonts/Roboto-LightItalic.ttf \
-       vendor/fonts-roboto/Roboto-Regular.ttf:system/fonts/Roboto-Regular.ttf
+       vendor/fonts-roboto/Roboto-Regular.ttf:system/fonts/Roboto-Regular.ttf \
+       vendor/fonts-roboto/Roboto-Thin.ttf:system/fonts/Roboto-Thin.ttf \
+       vendor/fonts-roboto/Roboto-ThinItalic.ttf:system/fonts/Roboto-ThinItalic.ttf \
+       vendor/fonts-roboto/RobotoCondensed-Bold.ttf:system/fonts/RobotoCondensed-Bold.ttf \
+       vendor/fonts-roboto/RobotoCondensed-BoldItalic.ttf:system/fonts/RobotoCondensed-BoldItalic.ttf \
+       vendor/fonts-roboto/RobotoCondensed-Italic.ttf:system/fonts/RobotoCondensed-Italic.ttf \
+       vendor/fonts-roboto/RobotoCondensed-Regular.ttf:system/fonts/RobotoCondensed-Regular.ttf
